2d914e27ce6dd2526c8f94662492a0a8651e02cf
saritavr24 <saritavallejor@gmail.com>
2024-11-04 16:55:36 -0500

    hola1

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-function laCajaDePandora(numero){
+function laCajaDePandora(numero) {
     if (numero % 2 === 0) { // Verificar si el número es par
         return numero.toString(2); // Convertir a binario
     } else { // Si es impar
         return numero.toString(16); // Convertir a hexadecimal
     }
-    }
+}
+
+function obtenerInformacion() {
+    return {
+        nombre: "Sarita",
+        edad: 25,
+        nacionalidad: "Colombiana"
+    };
+}
